75fd6a9579aaecf27d66090258e6508805b5747d
Elijah Shin <44964271+ElijahShin@users.noreply.github.com>
2023-11-13 15:50:42 -0800

    Complete 03_repeatString Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  if(string == "") return string;
 
+  const reverseStr = string.split('').reverse().join("");
+
+  return  reverseStr;
 };
 
 // Do not edit below this line
