e3ea14b7832cc81d3d90f19903bbc27379486ba8
Elijah Shin <44964271+ElijahShin@users.noreply.github.com>
2023-11-16 17:27:35 -0800

    Complete 05_sumAll exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+  let sumVal = 0;
+  let largeVal = 0;
 
+  let minusCheck = val1 < 0 || val2 < 0;
+  let numberTypeCheck = typeof val1 != "number" || typeof val2 != "number";
+
+  if (minusCheck || numberTypeCheck) {
+    return "ERROR";
+  }
+
+  //Check largest number
+  if ( val1 > val2) {
+    largeVal = val1;
+  } else {
+    largeVal = val2;
+  }
+
+  for (let i = 1; i <= largeVal; i++) {
+    sumVal += i;
+  }
+
+  return sumVal;
 };
 
 // Do not edit below this line
