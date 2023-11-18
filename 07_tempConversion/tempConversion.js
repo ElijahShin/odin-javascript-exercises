9b6445085d225b832e6d869e7d240f3b0e9d1034
Elijah Shin <44964271+ElijahShin@users.noreply.github.com>
2023-11-18 09:53:12 -0800

    Complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //Take fahrentheit
+  let toCelsius = (fahrenheit - 32) * (5/9);
+  let strToNum;
+
+  if (Number.isInteger(toCelsius)) {
+    return Math.trunc(toCelsius);
+  } 
+
+  strToNum = parseFloat(toCelsius.toFixed(1))
+
+  return strToNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let toFahrenheit = (celsius * (9/5) + 32);
+  let strToNum;
+
+  if (Number.isInteger(toFahrenheit)) {
+    return Math.trunc(toFahrenheit);
+  }
+
+  strToNum = parseFloat(toFahrenheit.toFixed(1))
+
+  return strToNum;
 };
 
 // Do not edit below this line
